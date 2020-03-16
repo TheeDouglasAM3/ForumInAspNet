7dcb6fe454b9831e668df23c494ea044efd7d570
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-16 19:42:32 -0300

    Atualiza Posts/Details.cshtml
    Adicionando a lista de comentários

--- ForumAspNetCore3.1/obj/Debug/netcoreapp3.1/ForumAspNetCore3.1.MvcApplicationPartsAssemblyInfo.cs
+++ ForumAspNetCore3.1/obj/Debug/netcoreapp3.1/ForumAspNetCore3.1.MvcApplicationPartsAssemblyInfo.cs
@@ -14,6 +14,9 @@ using System.Reflection;
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI.Views.V3")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI.Views.V4")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("ReflectionIT.Mvc.Paging")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("ReflectionIT.Mvc.Paging.Views")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
