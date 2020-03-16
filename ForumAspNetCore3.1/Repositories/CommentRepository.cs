2da29b93d681472a87c401d11af88531c96f1194
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-16 19:40:27 -0300

    Cria model de repositório para Comment

---
+++ ForumAspNetCore3.1/Repositories/CommentRepository.cs
@@ -0,0 +1,31 @@
+﻿using ForumAspNetCore3._1.Data;
+using ForumAspNetCore3._1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumAspNetCore3._1.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Comment>> GetAllFromPost(int? postId)
+        {
+            var commentsFromPost = await _context.Comment.Where(c => c.PostId == postId).Include(c => c.User).ToListAsync();
+            return commentsFromPost;
+        }
+
+        public Task<Comment> GetAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ForumAspNetCore3.1/Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using ForumAspNetCore3._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumAspNetCore3._1.Repositories
+{
+    public interface ICommentRepository
+    {
+        Task<Comment> GetAsync(int? id);
+        Task<List<Comment>> GetAllFromPost(int? postId);
+    }
+}
