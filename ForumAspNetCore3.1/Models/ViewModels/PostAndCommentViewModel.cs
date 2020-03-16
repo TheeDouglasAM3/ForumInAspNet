94d7b21868e975d531efa0bbd9bd9c369d328100
Douglas Marcelino <douglas_champion@hotmail.com>
2020-03-16 19:39:53 -0300

    Altera PostAndCommentViewModel
    Adicionando propriedade para o envio do comentário, e propriedade de lista dos comentários de um post X

--- ForumAspNetCore3.1/Models/ViewModels/PostAndCommentViewModel.cs
+++ ForumAspNetCore3.1/Models/ViewModels/PostAndCommentViewModel.cs
@@ -7,12 +7,14 @@ namespace ForumAspNetCore3._1.Models.ViewModels
 {
     public class PostAndCommentViewModel
     {
-        public PostAndCommentViewModel(Post post)
+        public PostAndCommentViewModel(Post post, List<Comment> comments)
         {
             Post = post;
+            Comments = comments;
         }
 
         public Post Post { get; set; }
-        public Comment Comment { get; set; }
+        public List<Comment> Comments { get; set; }
+        public Comment CommentToSend { get; set; }
     }
 }
